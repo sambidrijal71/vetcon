19a5c54f1d81b41db2cb61f9f8a8ced1762435ca
srijal <srijal@planittesting.com>
2023-02-01 20:23:44 +1000

    initial project skeleton

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import NavBar from '../components/NavBar'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+
+import React from 'react'
+
+export default function HomePage() {
+    return (
+        <div>
+            <NavBar />
+            <Header />
+            <Footer />
+        </div>
+    )
+}
